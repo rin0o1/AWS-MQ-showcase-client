4c5f9d3ac910fda652451e21c2b19e88530e184d
francescorinaldidev@gmail.com <francescorinaldi@ibm.com>
2022-09-26 12:48:23 +0100

    feat(tutorial): backend connection

---
+++ src/adapters/API.adapter.js
@@ -0,0 +1,33 @@
+import axios from 'axios';
+
+const END_POINT_GET_DEPTHS = 'http://9.20.193.145:8080/api/qdepth';
+const END_POINT_PUT = 'http://9.20.193.145:8080/api/mqput';
+const END_POINT_GET = 'http://9.20.193.145:8080/api/mqget?limit=';
+
+class APIAdapter {
+  async getAllDepths() {
+    let result = undefined;
+    try {
+      let _result = await axios.get(END_POINT_GET_DEPTHS);
+      result = _result.data;
+    } catch (e) {
+      console.log('Error on getting the queues info');
+    }
+
+    return result;
+  }
+
+  put(message, quantity) {
+    axios({
+      method: 'post',
+      url: END_POINT_PUT,
+      data: {
+        message: message,
+        quantity: quantity,
+      },
+    });
+  }
+  async getFromLimit(limit) {}
+}
+
+export default APIAdapter;
--- src/components/MQPatterns/PointToPoint/index.js
+++ src/components/MQPatterns/PointToPoint/index.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Grid, Column, Button } from '@carbon/react';
 import Chart1 from './chart1';
 import Chart2 from './chart2';
+import APIAdapter from '../../../adapters/API.adapter';
 import './index.scss';
 
 const DEFAULT_CHART_VALUE_CHART_2 = [
@@ -10,19 +11,13 @@ const DEFAULT_CHART_VALUE_CHART_2 = [
     key: 'Q1',
     value: 2500,
   },
-  {
-    group: 'Queue 2',
-    key: 'Q2',
-    value: 400,
-    label: 'hello',
-  },
 ];
 
 const DEFAULT_CHART_VALUE_CHART_1 = [
   {
-    group: 'Queue1',
+    group: 'Queue 1',
     time: '0',
-    value: 2500,
+    value: 0,
   },
 ];
 
@@ -30,24 +25,84 @@ const PointPointIndex = props => {
   const [dataChart1, setDataChart1] = useState(DEFAULT_CHART_VALUE_CHART_1);
   const [time, setTime] = useState(1);
   const [dataChart2, setDataChart2] = useState(DEFAULT_CHART_VALUE_CHART_2);
+  const [activeSender, setActiveSender] = useState();
+  const [activeReceiver, setActiveReceiver] = useState();
+
+  const adapter = new APIAdapter();
 
   useEffect(() => {
-    const interval = setInterval(() => {
-      let lastDepth = dataChart2[1]['value'];
+    if (activeSender) {
+      const interval = setInterval(async () => {
+        try {
+          adapter.put('Hello from client', 1);
+          let result = await adapter.getAllDepths();
+          updateChart(result);
+          console.log(result);
+        } catch (e) {
+          console.log(e);
+        }
+      }, 2000);
+      return () => clearInterval(interval);
+    }
+  });
+
+  function updateChart(result) {
+    if (result) {
       let _dataChart1 = [...dataChart1];
-      _dataChart1.push({
-        group: 'Queue1',
-        time: time,
-        value: lastDepth + 100,
-      });
       let _dataChart2 = [...dataChart2];
-      _dataChart2[1]['value'] = lastDepth + 100;
+
+      result.map(queue => {
+        // updating Chart1
+        let group = queue['name'];
+        let depth = parseInt(queue['depth']);
+        _dataChart1.push({
+          group: 'QUEUE' + group,
+          time: time,
+          value: depth * 10,
+        });
+
+        // updating Chart2
+        // if is not the first time the dataset is populated
+        if (dataChart2.length > 1) {
+          // for each queue update the last depth
+          let i = 0;
+          _dataChart2.forEach(e => {
+            if (e['key'] === queue['name']) {
+              _dataChart2[i]['value'] = parseInt(queue['depth']) * 10;
+            }
+            i = i + 1;
+          });
+        } else {
+          _dataChart2.push({
+            group: 'QUEUE' + group,
+            key: group,
+            value: depth,
+          });
+        }
+      });
       setDataChart2(_dataChart2);
       setDataChart1(_dataChart1);
       setTime(time + 1);
-    }, 500000);
-    return () => clearInterval(interval);
-  });
+    }
+  }
+
+  // useEffect(() => {
+  //   const interval = setInterval(() => {
+  //     let lastDepth = dataChart2[1]['value'];
+  //     let _dataChart1 = [...dataChart1];
+  //     _dataChart1.push({
+  //       group: 'Queue1',
+  //       time: time,
+  //       value: lastDepth + 100,
+  //     });
+  //     let _dataChart2 = [...dataChart2];
+  //     _dataChart2[1]['value'] = lastDepth + 100;
+  //     setDataChart2(_dataChart2);
+  //     setDataChart1(_dataChart1);
+  //     setTime(time + 1);
+  //   }, 2000);
+  //   return () => clearInterval(interval);
+  // });
 
   return (
     <Grid className="tabs-group-content">
@@ -65,10 +120,20 @@ const PointPointIndex = props => {
       <Column md={8} lg={16} sm={4} className="landing-page__r2">
         <Grid>
           <Column md={4} lg={{ offset: 1 }} sm={2}>
-            <Button> Active/Stop Sender </Button>
+            <Button
+              onClick={() => {
+                setActiveSender(!activeSender);
+              }}>
+              Active/Stop Sender
+            </Button>
           </Column>
           <Column md={4} lg={{ offset: 13 }} sm={2}>
-            <Button> Active/Stop Receiver </Button>
+            <Button
+              onClick={() => {
+                setActiveReceiver(!activeReceiver);
+              }}>
+              Active/Stop Receiver
+            </Button>
           </Column>
         </Grid>
         <Grid className="chartSection">
